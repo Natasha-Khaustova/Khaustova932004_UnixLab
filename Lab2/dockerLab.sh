a7645e442ddc9bcd9d133bc70284140d35962b0c
Natasha-Khaustova <45619280+Natasha-Khaustova@users.noreply.github.com>
2023-12-14 11:36:10 +0700

    Khaustova_Lab2

--- Lab2/dockerLab.sh
+++ Lab2/dockerLab.sh
@@ -1,37 +1,34 @@
 #!/bin/bash -e
 
-shared_directory="/_data"
-lock_file="/tmp/atomic_script.lock"
-temp_dir="/tmp/atomic_script_temp"
+shared_directory="/flock"
+lock_file="/flock/atomic_script"
 
 while true; do
     (
-        if flock -n 9; then  # Try to lock
-            # Generate random identifier for the container
-            container_id=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 10)
+        exec 9>"$lock_file"
+        flock 9
+        # Find the next available filename
+        for i in {001..999}; do
+            if [ ! -e "/$shared_directory/$i" ]; then
+                filename="$i"
+                break
+            fi
+        done
 
-            # Find the next available filename
-            counter=1
-            while true; do
-                filename=$(printf "%03d_%s" "$counter" "$container_id")
-                if [ ! -e "$shared_directory/$filename" ]; then
-                    break
-                fi
-                ((counter++))
-            done
+        flock -u 9
+        exec 9>&-
 
-            # Create and write to the file
-            echo "Container ID: $container_id" > "$shared_directory/$filename"
-            echo "File Number: $counter" >> "$shared_directory/$filename"
+        # Create and write to the file
+        echo "$(hostname) - $(date +%s)" > "/$shared_directory/$filename"
 
-            # Sleep for 1 second
-            sleep 1
+        # Sleep for 1 second
+        sleep 1
 
-            # Remove the file
-            rm "$shared_directory/$filename"
-        else
-            # Failed to acquire lock, sleep for a short time
-            sleep 0.1
-        fi
-    ) 9>"$lock_file"  # Release the lock
+        # Remove the file
+        rm "/$shared_directory/$filename"
+        
+        # Failed to acquire lock, sleep for a short time
+        sleep 0.1
+        
+    )
 done
